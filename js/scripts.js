8f7704cd749e293e84977516b6531e2a01d96a35
Soen Surya Soenaryo <wei.surya@gmail.com>
2017-08-12 11:42:17 +0700

    NPM Scripts Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,15 @@
-<script>
-    $(document).ready(function(){
-        $("#mycarousel").carousel( { interval: 2000 } );
-        $("#carousel-button").click(function(){
-            if ($("#carousel-button").children("span").hasClass('fa-pause')) {
-                $("#mycarousel").carousel('pause');
-                $("#carousel-button").children("span").removeClass('fa-pause');
-                $("#carousel-button").children("span").addClass('fa-play');
-            }
-            else if ($("#carousel-button").children("span").hasClass('fa-play')){
-                $("#mycarousel").carousel('cycle');
-                $("#carousel-button").children("span").removeClass('fa-play');
-                $("#carousel-button").children("span").addClass('fa-pause');                    
-            }
-        });
+$(document).ready(function(){
+    $("#mycarousel").carousel({interval:2000});
+    $("#carousel-button").click(function(){
+        if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass('fa-pause');
+            $("#carousel-button").children("span").addClass('fa-play');
+        }
+        else if ($("#carousel-button").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass('fa-play');
+            $("#carousel-button").children("span").addClass('fa-pause');                    
+        }
     });
-</script>
-
-/*Tooltip Script
-<script>
-    $(document).ready(function(){
-        $('[data-toggle="tooltip"').tooltip();
-    })
-</script>*/
+});
